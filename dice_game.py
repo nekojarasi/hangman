b9aa3723a4ff061323b47f4d28e91b2873b056bf
久多良木悠太 <kutaragiyuta@kutaragiyuutanoMacBook-ea.local>
2019-01-10 10:39:06 +0900

    my first commit

---
+++ dice_game.py
@@ -0,0 +1,19 @@
+import dices
+
+num = input('4,6,8,12,20のどれで勝負しますか？:')
+num = int(num)
+my_dice = dices.Dice(num)
+cpu_dice = dices.Dice(num)
+
+my_pip = my_dice.shoot()
+cpu_pip = cpu_dice.shoot()
+
+print('CPU:{}/あなた:{}'.format(cpu_pip, my_pip))
+
+if my_pip > cpu_pip:
+  print('おめでとうございます。あなたの勝ちです！')
+elif my_pip < cpu_pip:
+  print('残念！あなたの負けです。')
+else:
+  print('引き分けです。')
+  
